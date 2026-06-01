07bdac0458c907586fb7d9525c59f09015d7cc09
alwayshappyPyrik <81079168+alwayshappyPyrik@users.noreply.github.com>
2026-06-01 09:51:44 +0300

    hw-02
    перевел приложение на Annotation-based конфигурации, добавил пропертис файл, появился вывод результата теста студента, рефакторинг теста сервиса тестирования

---
+++ hw02-annotation-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,49 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CsvQuestionDao implements QuestionDao {
+
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        String fileName = fileNameProvider.getTestFileName();
+
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            if (is == null) {
+                throw new QuestionReadException(String.format("File not found in classpath: %s", fileName));
+            }
+
+            try (InputStreamReader reader = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+                CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
+                        .withSkipLines(1)
+                        .withType(QuestionDto.class)
+                        .withSeparator(';')
+                        .build();
+
+                return csvToBean.parse().stream()
+                        .map(QuestionDto::toDomainObject)
+                        .toList();
+
+            }
+        } catch (IOException e) {
+            throw new QuestionReadException(String.format("Failed to read questions from file: %s", fileName), e);
+        }
+    }
+}
