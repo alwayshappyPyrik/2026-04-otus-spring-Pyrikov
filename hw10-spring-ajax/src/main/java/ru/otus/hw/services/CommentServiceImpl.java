deb1ea9545843facf13a5e30b8e3851e40fbeaa4
Yaroslav Pyrikov <yarko597@gmail.com>
2026-08-10 17:31:01 +0300

    немного красоты еще добавил

--- hw10-spring-ajax/src/main/java/ru/otus/hw/services/CommentServiceImpl.java
+++ hw10-spring-ajax/src/main/java/ru/otus/hw/services/CommentServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.dto.CommentRequestDto;
 import ru.otus.hw.dto.CommentResponseDto;
-import ru.otus.hw.exceptions.EntityNotFoundException;
 import ru.otus.hw.exceptions.NotFoundException;
 import ru.otus.hw.mapper.CommentMapper;
 import ru.otus.hw.models.Book;
@@ -44,7 +43,7 @@ public class CommentServiceImpl implements CommentService {
         List<Comment> comments = commentRepository.findAllByBookId(bookId);
 
         if (comments.isEmpty()) {
-            throw new EntityNotFoundException("Comments for bookId %d not found".formatted(bookId));
+            throw new NotFoundException("Comments for bookId %d not found".formatted(bookId));
         }
 
         return comments.stream()
@@ -56,7 +55,7 @@ public class CommentServiceImpl implements CommentService {
     @Transactional
     public CommentResponseDto insert(CommentRequestDto commentRequestDto) {
         Book book = bookRepository.findById(commentRequestDto.book().id())
-                .orElseThrow(() -> new EntityNotFoundException("Book with id %d not found"
+                .orElseThrow(() -> new NotFoundException("Book with id %d not found"
                         .formatted(commentRequestDto.book().id())));
 
         Comment comment = Comment.builder()
@@ -72,7 +71,7 @@ public class CommentServiceImpl implements CommentService {
     @Transactional
     public CommentResponseDto update(CommentRequestDto commentRequestDto) {
         var comment = commentRepository.findById(commentRequestDto.id())
-                .orElseThrow(() -> new EntityNotFoundException("Comment with id %d not found"
+                .orElseThrow(() -> new NotFoundException("Comment with id %d not found"
                         .formatted(commentRequestDto.id())));
 
         comment.setText(commentRequestDto.text());
